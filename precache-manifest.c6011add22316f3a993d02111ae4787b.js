373240eb8095d4d422caff8cde7ca3f741f2262f
seunghyun-woo <shwoo9969@gmail.com>
2020-02-04 11:03:17 +0900

    Updates

---
+++ precache-manifest.c6011add22316f3a993d02111ae4787b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbc4cf590063db531ad939a5a1d41c4",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "e136cbd8a48ede85b1f6",
+    "url": "/movie_app/static/css/main.8484367b.chunk.css"
+  },
+  {
+    "revision": "70e84318d65e22caa855",
+    "url": "/movie_app/static/js/2.1c932c0d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app/static/js/2.1c932c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e136cbd8a48ede85b1f6",
+    "url": "/movie_app/static/js/main.61ac0945.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
